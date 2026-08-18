fd1658489abd21a90c01e6475ef48ca26b089787
dcabling14795 <dcabling@gmail.com>
2026-08-19 00:12:39 +0100

    remove when ticket is done

---
+++ ingestion/batch/te_sql_change.sql
@@ -0,0 +1,6 @@
+    SELECT *
+    FROM ccb1rep.ci_sa_char
+    
+    WHERE SA_ID = '3407296241'
+    ---WHERE char_type_cd IN ('TE-CODT', 'TE-FRMTL', 'TE-PHL', 'TE-PHU', 'TE-SLTTL', 'TE-TEMPR', 'TE_SPREF','CM-TECRD')
+    ORDER BY EFFDT DESC
